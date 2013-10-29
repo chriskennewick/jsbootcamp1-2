80d2f71a09ea9858920a57f5242f9f41b25c59b2
Chris Kennewick <chriskennewick@gmail.com>
2013-10-29 09:23:08 -0700

    fixing contact '/' bug

--- public/contactlist.js
+++ public/contactlist.js
@@ -3,7 +3,7 @@
 function makeContactRowClickable() {
   $(".table > tbody > tr").click(function() {
     var newUrl = window.location.href;
-    if(newUrl.charAt(newUrl.length) != "/") {
+    if(newUrl.charAt(newUrl.length-1) != "/") {
       newUrl = newUrl + "/";
     }
     newUrl = newUrl + $(this).data("contact-guid");
